fa8b6c8e15368198632bd25e3a5f022523eb0961
luanzd <diegoluandenil@hotmail.com>
2026-03-18 14:39:40 -0300

    Initial Commit: Project Ray Trancing

---
+++ include/rt3.hpp
@@ -0,0 +1,7 @@
+#include <string>
+#include <vector>
+
+struct RunningOpt {
+  std::vector<std::string> input_list;  //!< This might be a list of filenames or a directories.
+  // TODO: add other running options here.
+};
