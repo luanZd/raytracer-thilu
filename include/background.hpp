e0f7e36607c43efe33f24ab603890ddbfb21c5d5
luanzd <diegoluandenil@hotmail.com>
2026-03-24 08:33:31 -0300

    Background lerp fuction added

--- include/background.hpp
+++ include/background.hpp
@@ -1,12 +1,31 @@
-class Background
-{
+#ifndef BACKGROUND_HPP
+#define BACKGROUND_HPP
+
+#include <vector>
+#include <glm/vec3.hpp> 
+
+using Color = glm::vec3; //devemos substituir para uma struct (r,g,b)?
+
+class Background {
 private:
-    /* data */
-    int i{0}, j{0};
-    int background_color;
+    Color corners[4];
+    
+    enum Corners_e { 
+        bl = 0, // Bottom left
+        tl = 1, // Top left
+        tr = 2, // Top right
+        br = 3  // Bottom right
+    };
+
+    
+    Color lerp(const Color &A, const Color &B, float t) const;
+
 public:
-    Background(int i, int j);
-    ~Background();
+    Background(const std::vector<Color>& colors);
+    
+    ~Background() = default;
+
+    Color sampleUV(float u, float v) const;
 };
 
-
+#endif
