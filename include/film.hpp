c9b15e8105e01dab9ba7705c4b3f0e720edaed17
luanzd <diegoluandenil@hotmail.com>
2026-03-28 19:27:18 -0300

    Added film class

--- include/film.hpp
+++ include/film.hpp
@@ -1,13 +1,30 @@
+#ifndef FILM_HPP
+#define FILM_HPP
 
 #include <vector>
+#include <glm/vec3.hpp>
 
-class Film
-{
+using Color = glm::vec3;
+
+class Film {
 private:
-    /* data */
-    int width{0}, height{0};
+    int width;
+    int height;
+    
+   
     std::vector<Color> pixel_buffer;
 
 public:
     Film(int w, int h);
+    
+    ~Film() = default;
+
+    void draw_pixel(int x, int y, const Color& color);
+    Color get_pixel(int x, int y) const;
+    
+    //Getter funções
+    int get_width() const { return width; }
+    int get_height() const { return height; }
 };
+
+#endif
