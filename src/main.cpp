2f4bd60da87dd035a559755f8b5553144f8a951f
luanzd <diegoluandenil@hotmail.com>
2026-04-02 23:39:57 -0300

    main changed and add api.cpp to cmake

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,15 @@
-int main(){
+#include "rt3.hpp"
+#include "api.hpp"
+
+int main(int argc, char* argv[]) {
+    // 1. Lemos os argumentos do terminal (tu podes criar uma função para isto depois)
+    RunningOpt opt;
+    opt.infile = "scenes/scene01.xml"; // Simulação de ler do argv
+
+    // 2. O Ciclo de Vida da API ditado pelo professor
+    API::init_engine(opt);
+    API::run();
+    API::clean_up();
 
     return 0;
 }
