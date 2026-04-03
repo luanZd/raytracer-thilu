ccb46f0f46a70ce9101ece6123dbc6ddc4d9f551
luanzd <diegoluandenil@hotmail.com>
2026-04-02 23:11:04 -0300

    class utils added fuctions

--- include/class/util.cpp
+++ include/class/util.cpp
@@ -1,29 +1,38 @@
-#include "../util.hpp"
+#include "util.hpp"
+#include <algorithm> // Para o tolower no bool
 
 namespace Util {
 
     int parse_int(const std::string& value) {
-        // std::stoi é a função padrão do C++ (String TO Integer)
         return std::stoi(value);
     }
 
     float parse_float(const std::string& value) {
-        // std::stof (String TO Float)
         return std::stof(value);
+    }
+
+    bool parse_bool(const std::string& value) {
+        std::string val = value;
+        std::transform(val.begin(), val.end(), val.begin(), ::tolower);
+        return (val == "true" || val == "1" || val == "on");
     }
 
     Color parse_color(const std::string& value) {
         std::stringstream ss(value);
         int r = 0, g = 0, b = 0;
-        
-        // Tenta ler os três números do texto
         if (ss >> r >> g >> b) {
-            // Normaliza de 0-255 para 0.0-1.0
             return Color(r / 255.0f, g / 255.0f, b / 255.0f);
         }
-        
-        // Se falhar a leitura, devolve preto por segurança
         return Color(0.0f, 0.0f, 0.0f); 
     }
 
+    std::vector<float> parse_floats(const std::string& value) {
+        std::vector<float> result;
+        std::stringstream ss(value);
+        float val;
+        while (ss >> val) {
+            result.push_back(val);
+        }
+        return result;
+    }
 }
--- include/util.hpp
+++ include/util.hpp
@@ -2,22 +2,18 @@
 #define UTIL_HPP
 
 #include <string>
+#include <vector>
 #include <sstream>
 #include <glm/vec3.hpp>
 
 using Color = glm::vec3;
 
 namespace Util {
-    
-    // Converte um texto ("800") num inteiro (800)
     int parse_int(const std::string& value);
-
-    // Converte um texto ("3.14") num float (3.14f)
     float parse_float(const std::string& value);
-
-    // Converte texto ("255 128 0") numa Color normalizada
+    bool parse_bool(const std::string& value);
     Color parse_color(const std::string& value);
-
+    std::vector<float> parse_floats(const std::string& value);
 }
 
 #endif
